ae373d07b88ed4336965de4380e56b66862a5ec5
Ioan Plugar <ioanplugar@yandex.ru>
2022-02-06 10:58:54 +0300

    6-02

--- 15-13.py
+++ 15-13.py
@@ -2,3 +2,10 @@
 Напишите программу, которая находит значение суммы x + (x**2/2!) + ... + (x**n/n!). Программа получает на вход
 два натуральных числа x и n. Введённые числа не превышают 10.
 '''
+import math
+x = int(input())
+n = int(input())
+summa = x
+for i in range(2, n+1):
+    summa += (x ** i / math.factorial(i))
+print(round(summa, 2))
