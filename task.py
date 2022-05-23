9d835ae03c5a251895e7d6bd41369a53e4fa5df6
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2022-05-23 07:31:57 +0300

    23-05

--- task.py
+++ task.py
@@ -1,13 +1,20 @@
-number = input()
-s = []
-for i in range(len(number)-2):
-    s.append(number[i] + number[i + 1] + number[i + 2])
-maximum = 0
-for c in s:
-    summa = sum(map(int, c))
-    maximum = max(maximum, summa)
-print(maximum)
+with open('107_26.txt') as f:
+    n = int(f.readline())
+    d = dict()
+    for i in range(n):
+        row, mesto = map(int, f.readline().split())
+        if row in d:
+            d[row].append(mesto)
+        else:
+            d[row] = [mesto]
+for row in sorted(d):
+    d[row].sort()
+    for a, b in zip(d[row], d[row][1:]):
+        if b - a == 14:
+            print(row, a)
 
-'''
-and or not == <=
-'''
+
+
+
+
+
